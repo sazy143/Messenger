92d8a687ca628f236b096b202ce7be41aca5892f
Zachary Bentsen <bardown14@hotmail.com>
2019-11-05 15:12:15 -0700

    change path

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './MainPage.js';
-import { createBrowserHistory } from 'history';
-
-export const history = createBrowserHistory({
-    basename: process.env.PUBLIC_URL
-});
 
 ReactDOM.render(<Main />, document.getElementById('root'));
